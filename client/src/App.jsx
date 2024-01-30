34bc6cd21871e54deb67c7f228dfeadab2bcc53a
curious <vishal238128@gmail.com>
2024-01-30 13:31:04 +0530

    project initialize

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className="text-xl text-red-600">blog</h1>
+    </>
+  )
+}
+
+export default App
