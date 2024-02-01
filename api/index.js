60b8453bba6cbbf81a82810188b77c987fab00e0
curious <vishal238128@gmail.com>
2024-02-01 12:23:25 +0530

    Sign up, error, auth files added

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config();
 
@@ -10,6 +11,20 @@ mongoose
   .catch((err) => console.log(err));
 
 const app = express();
+
+app.use(express.json())
+
+app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
 
 app.listen(3000, () => {
   console.log("server is running on 3000");
